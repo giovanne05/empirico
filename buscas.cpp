2e31d7305115811c64dbe5c75199e413b978331c
GSDante <32875222+GSDante@users.noreply.github.com>
2018-03-10 15:06:25 -0300

    Update buscas.cpp

--- buscas.cpp
+++ buscas.cpp
@@ -37,3 +37,40 @@ int * biterativa( int *first, int *last, int value ) // busca binaria iterativa
 
     return last; // STUB
 }
+int * ternariait( int *first, int *last, int value ) // busca ternaria iterativa
+{
+
+	auto size = last - first;
+	auto m = ((size/3)-1);
+	auto t1 , t2;
+
+	t1 = (m + m);
+
+	if(value == first[m]){
+		return &first[m];
+	}else if( value < first[m]){
+		for( auto i{0}; i < m ; i++){
+			if (value == first[i]){
+				return &first [i];
+			}
+		}
+	}else if ( value > first[m] && value <= first[t1] ){
+		if(value = first[t1]){
+			return &first[t1];
+		}
+		for(auto i{m}; i < t1 ; i++){
+			if(value == first[i]){
+				return &first[i];
+			}
+		}
+	}else {
+		for( auto i{t1}; i <= size; i++ ){
+			if(value == first[i]){
+				return &first[i];
+			}
+		}
+	}
+
+	return last;
+
+}
