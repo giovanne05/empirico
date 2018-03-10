b8bb06ec53d23c86fc6e4fcd2ae735e0fa571839
Kevin Wallacy <kevinwall@ufrn.edu.br>
2018-03-09 21:48:06 -0300

    Adicionado o main.cpp abstrato e o funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+int * sliterativa( const int* , const int* , int );
+
+int * biterativa( const int*, const int*, int );
+
+int * linearR( const int*, const int*, int );
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "funcoes.h"
+
+int main()
+{
+	int i, size, numero;
+
+	std::vector<int> vetor
+
+	std::cout<<"Digite o tamanho do vetor para a busca: "<<std::endl;
+	std::cin>>size;
+
+	for(i = 0; i < size, i++)
+	{
+		vetor.push_back(i);
+	}
+
+	return 0;
+}
