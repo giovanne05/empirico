771ba5931ea7d3054794e8a9b08ee07aea9aac7c
GSDante <32875222+GSDante@users.noreply.github.com>
2018-03-18 08:47:11 -0300

    função main alterada

--- main.cpp
+++ main.cpp
@@ -1,37 +1,26 @@
-#include <iostream>
+include <iostream>
 #include "funcoes.h"
+#include <iterator>
+#include <vector>
 
 int main()
 {
-	int i, size, numero;
+	int  size, numero;
+	long int *first, *last,i,t;
 
-	//std::vector<int> vetor
+	t = 10000000;
 
-	//std::cout<<"Digite o tamanho do vetor para a busca: "<<std::endl;
-	//std::cin>>size;
+	std::vector<long int> v;
 
-	//for(i = 0; i < size, i++)
-	//{
-	//	vetor.push_back(i);
-	//}
-
-	long int v = {1, 3, 5, 6, 8, 9, 20, 34};
-
-	long int *first, *last;
-
-	first = &v[1];
-	last = v[7];
-
-	int result = fibonacci(first, last, 20);
-
-	std::cout<<result;
-
-	if(result == -1)
+	for( i = 0; i < t; i++)
 	{
-		std::cout<<"O número não está no vetor \n";
-	}else{
-		std::cout<<"O número foi encontrado na posição "<<result<<" do vetor \n";
-	}
+		v.push_back(i);
+	} 
+
+	pos = v.begin();
+
+	std::cout<<pos;
 
 	return 0;
 }
+
